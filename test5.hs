493ede19fe4d29e45e92a2a13399d1615d386d6c
unknown <gummi.bear456aaaaa@gmail.com>
2020-09-11 17:21:36 +0900

    必要なファイルのみを残す

---
+++ test5.hs
@@ -0,0 +1,13 @@
+import qualified System.Directory as SD
+
+notCDPD :: String -> Bool
+notCDPD "."   = False
+notCDPD ".."  = False
+notCDPD other = True
+
+main = do
+    currentDirectory <- SD.getCurrentDirectory
+    let targetDirectory = currentDirectory ++ "/enum"
+    list <- SD.getDirectoryContents targetDirectory
+    let filterdList = filter notCDPD list 
+    mapM_ putStrLn filterdList
