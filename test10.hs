1108e0d14c65bc58ffbfbc4429cff4f5d1382700
unknown <gummi.bear456aaaaa@gmail.com>
2020-09-15 02:30:10 +0900

    クラスのコメントを分割

---
+++ test10.hs
@@ -0,0 +1,16 @@
+import qualified Data.List as L
+
+requiredStr :: String -> Bool
+requiredStr x
+    | L.isInfixOf "class" x = True
+    | L.isInfixOf "//" x && L.isInfixOf "summary" x == False = True
+    | L.isInfixOf "," x = True
+    | otherwise = False
+
+main = do
+    str <- readFile "./enum/test.cs"
+    let splitedStr = lines str
+    let dropTopStr = dropWhile 
+    let filterdList = filter requiredStr splitedStr
+    let classComment = break (\str -> L.isInfixOf "class" str) filterdList
+    print classComment
